c3ea5bb7143abbded3bb7651e53906d5dab6e938
JCarlos1215 <167110309+JCarlos1215@users.noreply.github.com>
2024-05-07 14:38:55 -0600

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+const useImageURL = () => {
+  const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+               const responses = await Promise.all([
+          fetch("https://jsonplaceholder.typicode.com/photos/1", { mode: "cors" }),
+          fetch("https://jsonplaceholder.typicode.com/photos/2", { mode: "cors" })
+        ]);
+
+        const data = await Promise.all(responses.map(response => {
+          if (!response.ok) {
+            throw new Error("Server error!!");
+          }
+          return response.json();
+        }));
+
+      
+        const imageURLs = data.map(response => response.url);
+
+               setImageURL(imageURLs[0]);
+
+        
+        
+          setImageURL(imageURLs[1]);
+          setLoading(false);
+        
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { imageURL, error, loading };
+};
+
+function App() {
+  const { imageURL, error, loading } = useImageURL();
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>A network error was encountered!</p>
+
+  return (
+    imageURL && (
+      <>
+        <h1>An image</h1>
+        <img src={imageURL} alt={"placeholder text"} />
+      </>
+    )
+  )
+}
+
+export default App;
